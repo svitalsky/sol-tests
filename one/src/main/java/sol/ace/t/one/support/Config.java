af3e8221d7b241f11925f0a9796f1c573326a642
Marcel Svitalský <marcel.svitalsky@gmail.com>
2019-11-06 16:57:34 +0100

    Pretend Kerberos

--- one/src/main/java/sol/ace/t/one/support/Config.java
+++ one/src/main/java/sol/ace/t/one/support/Config.java
@@ -2,6 +2,7 @@ package sol.ace.t.one.support;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 import java.util.Properties;
 
 public enum Config {
@@ -32,5 +33,28 @@ public enum Config {
         catch (NumberFormatException e) {
             throw new IllegalArgumentException(String.format("Not an integer property: '%s'", key));
         }
+    }
+
+    public boolean getBoolProperty(String key) {
+        return Optional.ofNullable(properties.getProperty(key))
+                .map(s -> {
+                    boolean res;
+                    switch (s.trim().toLowerCase()) {
+                        case "0":
+                        case "n":
+                        case "no":
+                        case "f":
+                        case "false": res = false; break;
+                        case "1":
+                        case "y":
+                        case "yes":
+                        case "t":
+                        case "true": res = true; break;
+                        default: throw new IllegalArgumentException(String.format("Not a boolean property: '%s'", key));
+                    }
+                    return res;
+
+                })
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Not a boolean property: '%s'", key)));
     }
 }
--- one/src/main/java/sol/ace/t/one/support/SolOneTConnector.java
+++ one/src/main/java/sol/ace/t/one/support/SolOneTConnector.java
@@ -14,9 +14,15 @@ public class SolOneTConnector {
     private JCSMPProperties prepareProperties() {
         JCSMPProperties properties = new JCSMPProperties();
         properties.setProperty(JCSMPProperties.HOST, CONFIG.getProperty("solace.host"));
-        properties.setProperty(JCSMPProperties.USERNAME, CONFIG.getProperty("solace.username"));
         properties.setProperty(JCSMPProperties.VPN_NAME,  CONFIG.getProperty("solace.vpn"));
-        properties.setProperty(JCSMPProperties.PASSWORD, CONFIG.getProperty("solace.password"));
+        if (CONFIG.getBoolProperty("solace.kerberos")) {
+            properties.setProperty(JCSMPProperties.AUTHENTICATION_SCHEME, JCSMPProperties.AUTHENTICATION_SCHEME_GSS_KRB);
+            properties.setProperty(JCSMPProperties.KRB_SERVICE_NAME, CONFIG.getProperty("solace.kerberos.service-name"));
+        }
+        else {
+            properties.setProperty(JCSMPProperties.USERNAME, CONFIG.getProperty("solace.username"));
+            properties.setProperty(JCSMPProperties.PASSWORD, CONFIG.getProperty("solace.password"));
+        }
         return properties;
     }
 }
